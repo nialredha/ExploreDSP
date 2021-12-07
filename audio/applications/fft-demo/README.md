b6730ac88617a28dc7ca9ac66d4fac4041933a39
nialredha <nialredha@gmail.com>
2021-12-07 07:30:07 -0600

    started documentation

---
+++ audio/applications/fft-demo/README.md
@@ -0,0 +1,1 @@
+# Understanding the Discrete and Fast Fourier Transform
--- audio/applications/fft-demo/fft_main.c
+++ audio/applications/fft-demo/fft_main.c
@@ -1,57 +1,91 @@
+/*	FFT Demo/Explanation using the radix-2 algorithm implemented by me in C.
+	
+	The main goal here is to provide a decent explanation of what a fourier 
+	transform actually is and specifically how both the discrete and fast 
+	fourier transform work in general and in the context of the code that I 
+	wrote. 
+	
+	The secondary goal is to showcase my implementation of the FFT because
+	I am proud of it.
+
+	This is most certainly for my own sake - although I have a good 
+	understanding of fourier transforms, it is not good enough (meaning
+	I can't effectively explain it to someone else). So, that is what we are 
+	doing here, trying to explain to understand. 
+
+	This code is meant to be accompanied with the README file located in the
+	same directory this .c file is located. The point of this .c file is to 
+	give you the opportunity to engage with the algorithms and to showcase
+	the work that I have done. 
+
+	All things considered, I would like this reference to help others 
+	understand fourier transforms in mathematical and in computational terms.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+
+#include <math.h>	// Used for M_PI and for sin() and cos()
 #include <time.h>
 
-#include "../../modules/dataSim.h"
-#include "../../modules/fft.h"
+#include "../../modules/dataSim.h"	// module to simulate wave data
+#include "../../modules/fft.h"		// module to compute the transform
 
 int main() {
 
-	// FFT 
-	int N = 2048; //262144;	// radix-2 algorithm requires N be a power of 2
-	float data[N]; 
+	// variables used for measuring the speed of the transformtion
+	clock_t start, stop;
+	double cpu_time_used;
 
+	// (TODO): figure out what can be done about the buffer size limitation
+	// MAX Buffer Size = 262144
+	int N = 32;	// radix-2 algorithm requires N be a power of 2
+	
+	data_info d;	
+
+	d.frequency[0] = 30000.0;
+	d.frequency[1] = 600.0;
+	d.frequency[2] = 9000.0;
+	d.frequency[3] = 60.0;
+	d.num_frequencies = 1;
+
+	d.sample_rate = N;	
+	d.num_samples = d.sample_rate*1;	// 1 second duration
+	
+	d.noise_factor = 0.0;	// option to add noise not implemented yet 
+	d.num_channels = 0;		// important when working with .wav files	
+
+	// define and initialize data buffer to 0
+	float data[N]; 
 	for(int i=0; i<N; ++i) {
 		data[i] = 0.0;
 	}
 
-	clock_t start, stop;
-	double cpu_time_used;
-
-	struct data_info d;	
-	d.frequency[0] = 300.0;
-	d.frequency[1] = 600.0;
-	d.frequency[2] = 900.0;
-	d.frequency[3] = 60.0;
-	d.noise_factor = 0.0;
-	d.sample_rate = N;	//44100;
-	d.num_frequencies = 4;
-	d.num_samples = d.sample_rate*1;	/* 1 second duration */
-	d.num_channels = 0;	
-
-	//float frequency[] = 120.0;
-	//wave_gen(data, N, frequency);
-
+	// generate the data
    	float_multiple_sin(&d, data);
-/*	
+
+/*
 	for(int i=0; i<N; i++) {
 		printf("%f\n", data[i]);
 	}
 */
+
+	// transform the data and time it 
 	start = clock();
 	fft(data, N);
 	stop = clock();
 
+	// calculate the computation time 
 	cpu_time_used = ((double) (stop - start)) / CLOCKS_PER_SEC;
 
-/*
+	// print amplitude data in frequency domain
 	printf("Frequency Data:\n");
 	for(int i=0; i<N; i++) {
 		printf("%f\n", data[i]);
 	}
-*/
 
+	// determine the frequencies using transformed data by checking which
+	// index in the array has a significantly non-zero value (> 1.0)
 	printf("\n");
 	for(int i=0; i<N/2; i++) {
 		if(data[i] > 1.0) {
@@ -59,6 +93,7 @@ int main() {
 		}
 	}
 
+	// print the computation time it took to run the FFT
 	printf("\n");
 	printf("FFT Computation Time: %f\n", cpu_time_used);
 }
