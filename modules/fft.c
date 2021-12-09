03d0c9e85d195ba1dcf29426c21518b33651c285
nialredha <nialredha@gmail.com>
2021-12-09 11:23:58 -0600

    adding complex input/output capabilities and inverse fft

--- modules/fft.c
+++ modules/fft.c
@@ -7,25 +7,29 @@
 
 int NUMBER_OF_STAGES = 0;
 
-void dft(float* data, float* amp, int N) { 
+// (TODO): Finalize DFT - determine best suited inputs and outputs
+void dft(float* data, float* amp, int N) 
+{
+	float tau = (2*M_PI);	
+	float real = 0.0, imag = 0.0, sum_sqs = 0.0;
 
-	float tau = (2*M_PI)/N;	// part of the "angle" that remains constant
-	float real = 0, imag = 0, sum_sqs = 0;
-
-	float angle, new_angle, phase[N];
+	float omega, time, angle, phase[N];
 	
-	for(int k = 0; k<N; ++k) {
-		angle = tau*k;
+	for(int freq = 0; freq<N; ++freq) 
+	{
+		omega = tau*freq;
 
-		for(int n = 0; n<N; ++n) {
-			new_angle = angle*n;
+		for(int n = 0; n<N; ++n)
+		{
+			time = (float)n / (float)N;
+			angle = omega*time;
 
-			real += data[n] * cos(new_angle);
-			imag += data[n] * sin(new_angle);
+			real += data[n] * cos(angle);
+			imag += data[n] * sin(angle);
 		}
 
 		sum_sqs = real*real + imag*imag;
-		amp[k] = sqrt(sum_sqs);
+		amp[freq] = sqrt(sum_sqs);
 		//phase[k] = atan(imag/real);
 
 		real = 0; imag = 0;
@@ -33,13 +37,31 @@ void dft(float* data, float* amp, int N) {
 
 }
 
+void cdft(Complex *input, Complex *output, int N) 
+{
+	float tau = (2*M_PI);	
+	float omega, time, angle, phase[N];
+	
+	for(int freq = 0; freq<N; ++freq) 
+	{
+		omega = tau*freq;
+
+		for(int n = 0; n<N; ++n)
+		{
+			time = (float)n / (float)N;
+			angle = omega*time;
+
+			output->r[freq] += input->r[n] * cos(angle);
+			output->i[freq] += input->i[n] * sin(angle);
+		}
+	}
+}
+
+// (TODO): write comments :(
 void fft(float* data, int N)
 {
-	int order[N];
-	float sorted_data[N];
-	float sorted_data_i[N];
-	int rev = 0;
-	int log_N = N;
+	int order[N], rev = 0, log_N = N;
+	float sorted_data[N], sorted_data_i[N];
 
 	while (log_N > 0)
 	{
@@ -57,24 +79,20 @@ void fft(float* data, int N)
 		sorted_data_i[n] = 0;
 	}
 
-	for (int n = 0; n<N; ++n)
+	for (int n=0; n<N; ++n)
 	{
 		rev = reverse_bits(n, N);
 		order[n] = rev;
 	}
 
-	for (int n = 0; n<N; ++n)
+	for (int n=0; n<N; ++n)
 	{
 		sorted_data[n] = data[order[n]];
 	}
 
 	// INTIALIZATION 
-	int n = 0;
-	int inc = 1;
-	int k = 0;				// index for frequency data 
-	int k_step = 0;
-	int count = 0;
-
+	int n = 0, inc = 1, k = 0, k_step = 0, count = 0;
+	
 	float const_exp = 2.0*M_PI/N;
 
 	for (int s=0; s<NUMBER_OF_STAGES; ++s)
@@ -90,9 +108,9 @@ void fft(float* data, int N)
 			float top = sorted_data[n];
 			float top_i = sorted_data_i[n];
 			float bottom = sorted_data[n+inc] * cos(const_exp*k) + 
-							sorted_data_i[n+inc] * sin(const_exp*k);
+						   sorted_data_i[n+inc] * sin(const_exp*k);
 			float bottom_i = sorted_data_i[n+inc] * cos(const_exp*k) - 
-							  sorted_data[n+inc] * sin(const_exp*k);
+							 sorted_data[n+inc] * sin(const_exp*k);
 		
 			sorted_data[n] = top + bottom;
 			sorted_data[n+inc] = top - bottom; 
@@ -110,12 +128,12 @@ void fft(float* data, int N)
 			}
 			else 
 			{
-				n+=1;
+				n += 1;
 			}
 		}
 
-		inc = inc * 2; 
-		k_step = N/(2*inc);
+		inc *= 2; 
+		k_step = N / (2*inc);
 		n = 0;
 	}
 
@@ -125,6 +143,7 @@ void fft(float* data, int N)
 					  sorted_data_i[n]*sorted_data_i[n];
 		temp = sqrt(temp);
 		data[n] = temp;
+
 		//printf("%f\n", temp);
 	}
 
@@ -146,35 +165,120 @@ void fft(float* data, int N)
 	*/
 }
 
-/*
-void inverse_fft(struct Complex* input, struct Complex* output, int N)
+void cfft(Complex *input, Complex *output, int N)
 {
-	double inverse_N = 1 / (double)N;
+	int order[N], rev = 0, log_N = N;
+	float sorted_data[N], sorted_data_i[N];
 
-	for (int i=0; i<N; ++i) 
+	while (log_N > 0)
 	{
-		input->i[i] *= -1;	// intended to be the complex conjugate of the data
+		log_N >>= 1;
+		if (log_N != 0)
+		{
+			NUMBER_OF_STAGES += 1;
+		}
 	}
 
-	double_complex_fft(input, output, N);
+	for (int n=0; n<N; ++n)
+	{
+		order[n] = 0;
+		sorted_data[n] = 0;
+		sorted_data_i[n] = 0;
+	}
+
+	for (int n=0; n<N; ++n)
+	{
+		rev = reverse_bits(n, N);
+		order[n] = rev;
+	}
+
+	for (int n=0; n<N; ++n)
+	{
+		sorted_data[n] = input->r[order[n]];
+		sorted_data_i[n] = input->i[order[n]];
+	}
+
+	// INTIALIZATION 
+	int n = 0, inc = 1, k = 0, k_step = 0, count = 0;
+	
+	float const_exp = 2.0*M_PI/N;
+
+	for (int s=0; s<NUMBER_OF_STAGES; ++s)
+	{
+		// THE MAIN BUTTERFLY LOOP
+		//printf("Stage: %d\n", s);
+
+		while (n<N)
+		{
+			// top + (bottom * W^k)
+			// top - (bottom * W^k)
+			
+			float top = sorted_data[n];
+			float top_i = sorted_data_i[n];
+			float bottom = sorted_data[n+inc] * cos(const_exp*k) + 
+						   sorted_data_i[n+inc] * sin(const_exp*k);
+			float bottom_i = sorted_data_i[n+inc] * cos(const_exp*k) - 
+							 sorted_data[n+inc] * sin(const_exp*k);
+		
+			sorted_data[n] = top + bottom;
+			sorted_data[n+inc] = top - bottom; 
+			sorted_data_i[n] = top_i + bottom_i;
+			sorted_data_i[n+inc] = top_i - bottom_i;
+
+			k += k_step; 
+			count += 1;
+
+			if (count == inc)
+			{
+				n += (inc +1);
+				count = 0;
+				k = 0;
+			}
+			else 
+			{
+				n += 1;
+			}
+		}
+
+		inc *= 2; 
+		k_step = N / (2*inc);
+		n = 0;
+	}
+
+	for (int n=0; n<N; ++n)
+	{
+		output->r[n] = sorted_data[n];
+		output->i[n] = sorted_data_i[n];
+	}
+
+}
+
+void ifft(Complex* input, Complex* output, int N)
+{
+	float inverse_N = 1 / (float)N;
+	
+	for (int i=0; i<N; ++i) 
+	{
+		input->i[i] *= -1.0;	// intended to be the complex conjugate of the data
+		//printf("%f\n", input->r[i]);
+	}
+
+	cdft(input, output, N);
 
 	for (int i=0; i<N; ++i)
 	{
-		output->r[i] *= inverse_N;
-		output->i[i] *= inverse_N;
+		//printf("%f\n", output->i[i]);
 
-		//printf("%f\n", output->r[i]);
+		output->r[i] *= inverse_N;
+		output->i[i] *= -1.0 * inverse_N;
 	}
 }
-*/
 
 int reverse_bits(int num, int N)
 {
 	/* return bit reversed order */
 
-	int rev = 0;
-	int count = 0;
-	int max_count = NUMBER_OF_STAGES;
+	int rev = 0, count = 0, max_count = NUMBER_OF_STAGES;
 
 	while (count < max_count)
 	{
@@ -190,6 +294,7 @@ int reverse_bits(int num, int N)
 	return rev; 
 } 
 
+// (TODO): Delete this
 void wave_gen(float *data, int N, float freq) { 
 	//float freq = 1.0;						// cycles per second 
 	int s_freq = N;
--- modules/fft.h
+++ modules/fft.h
@@ -1,18 +1,20 @@
-#ifndef FFT2_H_	// include guard
+#ifndef FFT_H_	// include guard
 #define FFT_H_
 
 typedef struct Complex
 {
-	double* i;
-	double* r;
+	float* i;
+	float* r;
 } Complex;
 
 // function declarations 
-int		reverse_bits(int num, int N);				// bit reversed order calc
+int		reverse_bits(int num, int N);					// bit reversed order calc
 
-void	dft(float* data, float* amp, int N);		// discrete fourier transform
-void	fft(float* data, int N);					// radix-2 algorithm 
-void	inverse_fft(struct Complex *input, struct Complex *output, int N);						
+void	dft(float* data, float* amp, int N);			// discrete fourier transform
+void	cdft(Complex *input,  Complex *output, int N);	// discrete fourier transform
+void	fft(float* data, int N);						// radix-2 algorithm 
+void	cfft(Complex *input, Complex *output, int N);
+void	ifft(Complex *input, Complex *output, int N);						
 
 // Soon to either be deleted or restored
 void	wave_gen(float *data, int N, float freq);	// wave generation for testing
