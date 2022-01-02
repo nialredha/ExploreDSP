c9b569048cbc26e8da0b5a111994470403b63b9c
nialredha <nialredha@gmail.com>
2022-01-02 10:35:25 -0600

    changed build command

--- applications/reverberation/FBCF.c
+++ applications/reverberation/FBCF.c
@@ -1,5 +1,5 @@
 /* 
-	Build Command: gcc FBCF.c ../../modules/filters.c ../../modules/wav.c -lm	
+	Build Command: gcc FBCF.c ../../modules/filters.c ../../modules/wav.c ../../modules/fft.c -lm	
 	Run Command: ./a.out
 */
 
