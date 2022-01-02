4e27d7b3236fe4b9303fca5edc6a71159a6f5f71
nialredha <nialredha@gmail.com>
2022-01-02 10:34:53 -0600

    changed build command

--- applications/reverberation/SR.c
+++ applications/reverberation/SR.c
@@ -1,5 +1,5 @@
 /* 
-	Build Command: gcc SR.c ../../modules/filters.c ../../modules/wav.c -lm	
+	Build Command: gcc SR.c ../../modules/filters.c ../../modules/wav.c ../../modules/fft.c -lm	
 	Run Command: ./a.out
 */
 
