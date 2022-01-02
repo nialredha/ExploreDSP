dfb3b6b747bffe229b1b64c24cd3b4cd4215392b
nialredha <nialredha@gmail.com>
2022-01-02 11:54:11 -0600

    changed build command

--- applications/reverberation/DL_test.c
+++ applications/reverberation/DL_test.c
@@ -2,7 +2,7 @@
 #include "../../modules/filters.h"
 #include "../../modules/dataSim.h"
 
-// gcc delay_line_test.c ../../modules/filters.c ../../modules/dataSim.c -lm
+// gcc DL_test.c ../../modules/filters.c ../../modules/dataSim.c ../../modules/fft.c -lm
 
 int main() 
 {
